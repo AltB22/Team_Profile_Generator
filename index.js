d5727cb0ce9a3302403c252acf5a4328402ffe35
Alt B 2022 <massie.w@gmail.com>
2023-01-31 12:33:38 -0800

    created folder structure with liv, utils, and index at root.  Created classes of Employee and Manager.  Worked on start function and addManager.

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');//const to require installation of inquirer
+const fs = require('fs');//indicating interaction with the file system
+const generateTeamProfile = require('./utils/generateTeamProfile');
+const Manager = require('./lib/Manager');
+
+const team =  [];
+
+function start(){
+    inquirer.prompt([
+        {
+          type: "list",//could be list of types of employees
+          name: "employeeType",
+          message: "What is your name?",
+          choices: ['Manager', 'Engineer', 'Intern', 'Quit'] 
+        },
+    ]).then(function(answer){
+        if(answer.employeeType === "Manager"){
+            addManager();
+        } else if (answer.employeeType === "Engineer"){
+            addEngineer();
+        } else if (answer.employeeType === "Intern"){
+            addIntern();
+        } else {
+            createPage()//
+        }
+    })
+}
+
+function addManager(){
+    inquirer.prompt([
+        {
+          type: "input",//could be list of types of employees
+          name: "name",
+          message: "What is your name?",
+        },
+    ]).then(function (managerData){
+        console.log(managerData)
+        const newManager = new Manager(
+            managerData.name
+        )
+        console.log(newManager)
+        team.push(newManager)
+        start()
+    })
+}
+
+
+
+start()
