4671977b9ce1208c53a620845c1f7aa9a858b683
Alt B 2022 <massie.w@gmail.com>
2023-02-22 22:35:52 -0800

    finshed HTML template for overall page.  Can now test with Manager Card

--- src/generateTeamProfile.js
+++ src/generateTeamProfile.js
@@ -19,3 +19,31 @@ const generateManager = function (manager) {
 `;
 }
 
+const generateTeamProfilePageHTML = function(employeeCards){
+return`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <span>My Team Profile</span>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row jusfify-content-center" id="team-cards">
+                ${employeeCards}
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
+`;
+}
